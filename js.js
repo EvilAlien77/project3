57d1a109a8196f607837bba21821aa6f48edf8c8
EvilAlien <evilalien37@gmail.com>
2018-04-30 13:19:40 +0300

    fix menu

--- js.js
+++ js.js
@@ -9,6 +9,30 @@ $('.sl').slick({
 });
 
 $('.side-icon').click(function () {
-	$('.home').toggle();
+	$('.side').toggle();
 
 });
+$('.side-icon2').click(function () {
+    $('.side-2').toggle();
+
+});
+$('.li-span11').click(function () {
+    $('.sub-1').toggle();
+
+});
+$('.li-span12').click(function () {
+    $('.sub-2').toggle();
+
+});
+/*$('.slider').slick({
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    speed:1000,
+    arrows: true,
+    dots: false,
+    fade:false,
+    pauseOnHover: false,
+    slidesToShow: 4,
+    slidesToScroll: 1
+});*/

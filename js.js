0efa3cd252828e4169d2deec65ea3d65f5c48588
dmitriy.p@hostzealot.com <dmitriy.p@hostzealot.com>
2018-05-05 14:57:20 +0300

    kumec commit
    kumec commit

--- js.js
+++ js.js
@@ -10,51 +10,56 @@
 	pauseOnHover: false,
 
 });*/
-if(window.matchMedia('(max-width: 900px)').matches)
-{
-    $('.sl').slick({
-        autoplay: false,
-        autoplaySpeed: 4000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed:1000,
-        arrows: false,
-        dots:true,
-        fade:false,
-        pauseOnHover: false,
+$(document).ready(function(){
+
+    if(screen.width <= 900){
+        $('.sl').slick({
+            autoplay: false,
+            autoplaySpeed: 4000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed:1000,
+            arrows: false,
+            dots:true,
+            fade:false,
+            pauseOnHover: false,
+
+        });
+    } else {
+
+        console.log('here');
+        $('.sl').slick({
+            autoplay: false,
+            autoplaySpeed: 4000,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            speed:1000,
+            arrows: false,
+            dots:true,
+            fade:false,
+            pauseOnHover: false,
+
+        });
+    }
+
+    $('.side-icon').click(function () {
+        $('.side').toggle();
 
     });
-} else {
-    $('.sl').slick({
-        autoplay: false,
-        autoplaySpeed: 4000,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        speed:1000,
-        arrows: false,
-        dots:true,
-        fade:false,
-        pauseOnHover: false,
+    $('.side-icon2').click(function () {
+        $('.side-2').toggle();
 
     });
-}
+    $('.li-span11').click(function () {
+        $('.sub-1').toggle();
 
-$('.side-icon').click(function () {
-	$('.side').toggle();
+    });
+    $('.li-span12').click(function () {
+        $('.sub-2').toggle();
 
+    });
 });
-$('.side-icon2').click(function () {
-    $('.side-2').toggle();
 
-});
-$('.li-span11').click(function () {
-    $('.sub-1').toggle();
-
-});
-$('.li-span12').click(function () {
-    $('.sub-2').toggle();
-
-});
 /*$('.slider').slick({
     infinite: false,
     autoplay: false,
